69e3d00b859e7e81a7a605d76c5249a2a23402a3
Sebastien P <maxymeum.prod@gmail.com>
2010-12-30 11:28:03 +0100

    New methods added

--- jquery.hasEventListener-1.0.2.js
+++ jquery.hasEventListener-1.0.2.js
@@ -17,8 +17,10 @@
 
    - $.hasEventListener(dom_element, "event"); // true or false
    - $.hasEventListener(dom_element, "event.namespace"); // true or false
+   - $("selector:hasEventListener"); // jQuery chainable object
    - $("selector:hasEventListener(event)"); // jQuery chainable object
    - $("selector:hasEventListener(event.namespace)"); // jQuery chainable object
+   - $("selector").hasEventListener(); // jQuery chainable object
    - $("selector").hasEventListener("event"); // jQuery chainable object
    - $("selector").hasEventListener("event.namespace"); // jQuery chainable object
 
@@ -29,37 +31,61 @@
 
     "use strict";
 
+    function is_plain_string(argument) {
+
+        return (argument && ($.type(argument) === "string") && $.trim(argument));
+
+    }
+
+
     /* $.hasEventListener(dom_element, event_name); */
 
     $[PLUGIN_NAME] = function (element, event_name) {
 
         var event_listeners = $.data(element, "events"),
+        to_return = false,
         event_namespace,
         found_namespace,
         iterator;
 
-        if (
-            event_listeners &&
-            // RegExp for "event" or "event.namespace" (az.azAZ09_-).
-            (event_name = /^([a-z]+)(\.([\w\d\-]+))?$/.exec(event_name)) &&
-            (event_listeners = event_listeners[event_name[1]])
-        ) {
+        if (event_listeners) {
 
             if (
-                (event_namespace = event_name[3])
+                is_plain_string(event_name) &&
+                // RegExp for "event" or "event.namespace" (az.azAZ09_-).
+                (event_name = /^([a-z]+)(\.([\w\d\-]+))?$/.exec(event_name))
             ) {
 
-                iterator = event_listeners.length;
-
-                while ((iterator -= 1) >= 0) {
+                if (
+                    (event_listeners = event_listeners[event_name[1]])
+                ) {
 
                     if (
-                        (found_namespace = event_listeners[iterator].namespace) &&
-                        (found_namespace.split(".")[0] === event_namespace)
+                        (event_namespace = event_name[3])
                     ) {
 
-                        // There is a such event.namespaced bound.
-                        return TRUE;
+                        iterator = event_listeners.length;
+
+                        while ((iterator -= 1) >= 0) {
+
+                            if (
+                                (found_namespace = event_listeners[iterator].namespace) &&
+                                (found_namespace.split(".")[0] === event_namespace)
+                            ) {
+
+                                // There is a such event.namespaced bound.
+                                to_return = TRUE;
+
+                                break;
+
+                            }
+
+                        }
+
+                    } else {
+
+                        // There is a such event (not namespaced) bound.
+                        to_return = TRUE;
 
                     }
 
@@ -67,15 +93,15 @@
 
             } else {
 
-                // There is a such event (not namespaced) bound.
-                return TRUE;
+                // There is at least one event bound.
+                to_return = TRUE;
 
             }
 
         }
 
         // There is no such event or event.namespaced bound.
-        return false;
+        return to_return;
 
     };
 
@@ -93,7 +119,7 @@
 
     $.fn[PLUGIN_NAME] = function (event_name) {
 
-        return this.filter(":" + PLUGIN_NAME + "(" + event_name + ")");
+        return this.filter(":" + PLUGIN_NAME + ((is_plain_string(event_name)) ? "(" + event_name + ")" : ""));
 
     };
 
